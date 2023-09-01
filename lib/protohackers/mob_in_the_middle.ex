bfe0c82109da1fee78bc855b8b5859c96d33c83b
Damien Gonot <damien.gonot@gmail.com>
2023-09-01 11:03:52 -0400

    mob in the middle rewrite

--- lib/protohackers/mob_in_the_middle.ex
+++ lib/protohackers/mob_in_the_middle.ex
@@ -5,40 +5,43 @@ defmodule Protohackers.MobInTheMiddle do
   Starts accepting connections on the given `port`.
   """
   def accept(port) do
-    {:ok, socket} =
-      :gen_tcp.listen(
-        port,
-        [:binary, packet: :line, active: false, reuseaddr: true]
-      )
-
-    {:ok, upstream_socket} =
-      :gen_tcp.connect("chat.protohackers.com", 16963, [:binary, packet: :line])
+    {:ok, listen_socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
     Logger.info("Accepting connections on port #{port}")
-    loop_acceptor(socket, upstream_socket)
+
+    loop_accept(listen_socket)
   end
 
-  defp loop_acceptor(socket, upstream_socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
-
-    {:ok, pid} =
-      Task.Supervisor.start_child(Protohackers.TaskSupervisor, fn ->
-        serve(client, upstream_socket)
-      end)
-
-    :ok = :gen_tcp.controlling_process(client, pid)
-    loop_acceptor(socket, upstream_socket)
+  defp loop_accept(listen_socket) do
+    {:ok, client_socket} = :gen_tcp.accept(listen_socket)
+    spawn_link(fn -> handle_client(client_socket) end)
+    loop_accept(listen_socket)
   end
 
-  defp serve(socket, upstream_socket) do
-    case :gen_tcp.recv(socket, 0) do
+  defp handle_client(client_socket) do
+    {:ok, upstream_socket} =
+      :gen_tcp.connect('chat.protohackers.com', 16963, [:binary, packet: :line, active: false])
+
+    spawn_link(fn -> relay_messages(client_socket, upstream_socket) end)
+    relay_messages(upstream_socket, client_socket)
+  end
+
+  defp relay_messages(from_socket, to_socket) do
+    case :gen_tcp.recv(from_socket, 0) do
       {:ok, data} ->
-        :gen_tcp.send(upstream_socket, data) |> IO.inspect()
-        :gen_tcp.send(socket, data)
-        serve(socket, upstream_socket)
+        transformed_data = replace_substring(data)
+        :gen_tcp.send(to_socket, transformed_data)
+        relay_messages(from_socket, to_socket)
 
-      {:error, :closed} ->
-        :gen_tcp.close(socket)
+      {:error, _} ->
+        :gen_tcp.close(from_socket)
+        :gen_tcp.close(to_socket)
     end
+  end
+
+  defp replace_substring(data) do
+    regex = ~r/(?<=^| )7[a-zA-Z0-9]{25,34}(?=$| )/
+    String.replace(data, regex, "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
   end
 end
